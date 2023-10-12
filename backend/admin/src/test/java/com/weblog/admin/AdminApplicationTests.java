6be318c514cfa366058c25b8c84ec56312b2e275
arronf2e <arronf2e@163.com>
2023-10-12 21:48:07 +0800

    feat: 添加后端项目三模块

---
+++ backend/admin/src/test/java/com/weblog/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
