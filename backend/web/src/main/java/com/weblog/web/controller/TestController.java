c76a3e03424d203805a61e2fe4675e217cdcff9a
arronf2e <arronf2e@163.com>
2023-10-13 19:28:10 +0800

    feat: 自定义了 Jackson 序列化、反序列化，让其支持了 Java 8 新的日期

--- backend/web/src/main/java/com/weblog/web/controller/TestController.java
+++ backend/web/src/main/java/com/weblog/web/controller/TestController.java
@@ -3,6 +3,7 @@ package com.weblog.web.controller;
 import com.weblog.common.aspect.ApiOperationLog;
 import com.weblog.common.enums.ResponseCodeEnum;
 import com.weblog.common.exception.BizException;
+import com.weblog.common.utils.JsonUtil;
 import com.weblog.common.utils.Response;
 import com.weblog.web.model.User;
 import io.swagger.annotations.Api;
@@ -13,6 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @RestController
 @Slf4j
 @Api(tags = "首页模块")
@@ -20,7 +25,15 @@ public class TestController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     @ApiOperation(value = "测试接口")
-    public Response test(@RequestBody @Validated User user) {
-        return Response.success();
+    public Response test(@RequestBody User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
     }
 }
--- backend/web/src/main/java/com/weblog/web/model/User.java
+++ backend/web/src/main/java/com/weblog/web/model/User.java
@@ -1,14 +1,23 @@
 package com.weblog.web.model;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
+@ApiModel(value = "用户实体类")
 public class User {
     // 用户名
     @NotBlank(message = "用户名不能为空")
     private String username;
     // 性别
     private Integer sex;
+
+    private LocalDateTime createTime;
+    private LocalDate updateDate;
+    private LocalTime time;
 }
