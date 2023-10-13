706265b66482cbe2038f7967f3cd76c6c44b5f1b
arronf2e <arronf2e@163.com>
2023-10-13 14:17:29 +0800

    feat: api请求日志切面配置

---
+++ backend/web/src/main/java/com/weblog/web/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.weblog.web.controller;
+
+import com.weblog.common.aspect.ApiOperationLog;
+import com.weblog.web.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+    @PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public User test(@RequestBody User user) {
+        return user;
+    }
+}
