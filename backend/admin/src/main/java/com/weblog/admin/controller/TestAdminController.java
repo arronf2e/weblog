63f7f35c6e06f91e6e0b1b408126e4135aa96442
arronf2e <arronf2e@163.com>
2023-10-13 19:17:15 +0800

    feat: add knife4j doc

--- backend/admin/src/main/java/com/weblog/admin/controller/TestAdminController.java
+++ backend/admin/src/main/java/com/weblog/admin/controller/TestAdminController.java
@@ -1,26 +1,23 @@
-package com.weblog.web.controller;
+package com.weblog.admin.controller;
 
 import com.weblog.common.aspect.ApiOperationLog;
-import com.weblog.common.enums.ResponseCodeEnum;
-import com.weblog.common.exception.BizException;
 import com.weblog.common.utils.Response;
-import com.weblog.web.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 @Slf4j
 @Api(tags = "首页模块")
-public class TestController {
+public class TestAdminController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     @ApiOperation(value = "测试接口")
-    public Response test(@RequestBody @Validated User user) {
+    public Response test() {
         return Response.success();
     }
 }
